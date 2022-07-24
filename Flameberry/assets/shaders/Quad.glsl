dfec1635e6eab0ad4b5b71902792ba266c5cf37c
Flameberry <flameberrydev@gmail.com>
2022-07-24 22:48:55 +0530

    Update Quad.glsl

--- Flameberry/assets/shaders/Quad.glsl
+++ Flameberry/assets/shaders/Quad.glsl
@@ -6,15 +6,11 @@ layout (location = 1) in vec4 a_Color;
 layout (location = 2) in vec2 a_Texture_UV;
 layout (location = 3) in float a_TextureIndex;
 layout (location = 4) in vec2 a_QuadDimensions;
-layout (location = 5) in float a_ElementTypeIndex;
-layout (location = 6) in float a_IsPanelActive;
 
 out vec4 v_Color;
 out float v_TextureIndex;
 out vec2 v_Texture_UV;
 out vec2 v_QuadDimensions;
-out float v_ElementTypeIndex;
-out float v_IsPanelActive;
 
 layout (std140) uniform Camera
 {
@@ -27,8 +23,6 @@ void main()
     v_TextureIndex = a_TextureIndex;
     v_Texture_UV = a_Texture_UV;
     v_QuadDimensions = a_QuadDimensions;
-    v_ElementTypeIndex = a_ElementTypeIndex;
-    v_IsPanelActive = a_IsPanelActive;
 
     gl_Position = u_Camera.ViewProjectionMatrix * vec4(a_Position, 1.0);
 }
@@ -42,8 +36,6 @@ in vec4 v_Color;
 in float v_TextureIndex;
 in vec2 v_Texture_UV;
 in vec2 v_QuadDimensions;
-in float v_ElementTypeIndex;
-in float v_IsPanelActive;
 
 uniform sampler2D u_TextureSamplers[16];
 
