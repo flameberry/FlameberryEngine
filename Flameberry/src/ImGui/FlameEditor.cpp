4fc970c366c2c654f64fc074d8c59d1934e11b85
Flameberry <flameberrydev@gmail.com>
2022-08-02 19:53:03 +0530

    Added FPS counter to ImGui Settings Window.

--- Flameberry/src/ImGui/FlameEditor.cpp
+++ Flameberry/src/ImGui/FlameEditor.cpp
@@ -19,6 +19,7 @@ namespace Flameberry {
         ImGui::PopStyleVar();
 
         ImGui::Begin("Settings");
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         ImGui::End();
     }
 
