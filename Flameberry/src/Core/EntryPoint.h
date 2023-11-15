bedc8bc791559c4975702cd89f0891e12269b620
Aditya Gaikwad <flameberry@Adityas-MacBook-Pro.local>
2023-11-15 10:15:07 +0530

    Made creation and deletion of clientApp explicit by using new instead of shared_ptr.

--- Flameberry/src/Core/EntryPoint.h
+++ Flameberry/src/Core/EntryPoint.h
@@ -8,9 +8,9 @@ int main(int argc, char const* argv[])
     std::shared_ptr<Flameberry::Logger> logger = Flameberry::Logger::Create("FLAMEBERRY");
     Flameberry::Logger::SetCoreLogger(logger);
     logger->SetLogLevel(Flameberry::LogLevel::LOG);
-
-    auto clientApp = Flameberry::Application::CreateClientApp();
+    
+    Flameberry::Application* clientApp = Flameberry::Application::CreateClientApp();
     clientApp->Run();
-
+    delete clientApp;
     return 0;
 }
