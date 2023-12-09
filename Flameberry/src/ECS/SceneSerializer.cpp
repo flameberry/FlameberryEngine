a84acc233117795b5122f21feaeb1041da48325a
Flameberry <flameberrydev@gmail.com>
2023-12-09 17:33:49 +0530

    Added ability to save and load RelationshipComponent.

--- Flameberry/src/ECS/SceneSerializer.cpp
+++ Flameberry/src/ECS/SceneSerializer.cpp
@@ -89,127 +89,141 @@ namespace Flameberry {
         };
 
         destScene->m_Name = data["Scene"].as<std::string>();
-        
+
         destScene->m_Registry->clear();
 
         auto entities = data["Entities"];
         if (entities)
         {
-            for (auto entity : entities)
+            std::unordered_map<UUID, fbentt::entity> UUIDToEntityMap;
+            UUIDToEntityMap[UUID(0)] = fbentt::null;
+            for (const auto entity : entities)
             {
-                if (entity)
+                const auto deserializedEntity = destScene->m_Registry->create();
+                const UUID ID = entity["Entity"].as<uint64_t>();
+                UUIDToEntityMap[ID] = deserializedEntity;
+            }
+
+            // Deserialize entities
+            for (const auto entity : entities)
+            {
+                const UUID ID = entity["Entity"].as<uint64_t>();
+                const fbentt::entity deserializedEntity = UUIDToEntityMap[ID];
+
+                auto& IDComp = destScene->m_Registry->emplace<IDComponent>(deserializedEntity);
+                IDComp.ID = ID;
+
+                if (auto tag = entity["TagComponent"]; tag)
                 {
-                    // Deserialize entity
-                    auto deserializedEntity = destScene->m_Registry->create();
-                    if (auto id = entity["Entity"]; id)
-                    {
-                        auto& IDComp = destScene->m_Registry->emplace<IDComponent>(deserializedEntity);
-                        IDComp.ID = id.as<uint64_t>();
-                    }
+                    auto& tagComp = destScene->m_Registry->emplace<TagComponent>(deserializedEntity);
+                    tagComp.Tag = tag.as<std::string>();
+                }
 
-                    if (auto tag = entity["TagComponent"]; tag)
-                    {
-                        auto& tagComp = destScene->m_Registry->emplace<TagComponent>(deserializedEntity);
-                        tagComp.Tag = tag.as<std::string>();
-                    }
+                if (auto transform = entity["TransformComponent"]; transform)
+                {
+                    auto& transformComp = destScene->m_Registry->emplace<TransformComponent>(deserializedEntity);
+                    transformComp.Translation = transform["Translation"].as<glm::vec3>();
+                    transformComp.Rotation = transform["Rotation"].as<glm::vec3>();
+                    transformComp.Scale = transform["Scale"].as<glm::vec3>();
+                }
 
-                    if (auto transform = entity["TransformComponent"]; transform)
-                    {
-                        auto& transformComp = destScene->m_Registry->emplace<TransformComponent>(deserializedEntity);
-                        transformComp.Translation = transform["Translation"].as<glm::vec3>();
-                        transformComp.Rotation = transform["Rotation"].as<glm::vec3>();
-                        transformComp.Scale = transform["Scale"].as<glm::vec3>();
-                    }
+                if (auto relation = entity["RelationshipComponent"]; relation)
+                {
+                    auto& relationComp = destScene->m_Registry->emplace<RelationshipComponent>(deserializedEntity);
+                    relationComp.Parent = UUIDToEntityMap[relation["Parent"].as<uint64_t>()];
+                    relationComp.PrevSibling = UUIDToEntityMap[relation["PrevSibling"].as<uint64_t>()];
+                    relationComp.NextSibling = UUIDToEntityMap[relation["NextSibling"].as<uint64_t>()];
+                    relationComp.FirstChild = UUIDToEntityMap[relation["FirstChild"].as<uint64_t>()];
+                }
 
-                    if (auto camera = entity["CameraComponent"]; camera)
-                    {
-                        auto& cameraComp = destScene->m_Registry->emplace<CameraComponent>(deserializedEntity);
-                        cameraComp.IsPrimary = camera["IsPrimary"].as<bool>();
+                if (auto camera = entity["CameraComponent"]; camera)
+                {
+                    auto& cameraComp = destScene->m_Registry->emplace<CameraComponent>(deserializedEntity);
+                    cameraComp.IsPrimary = camera["IsPrimary"].as<bool>();
 
-                        ProjectionType type = ProjectionTypeStringToEnum(camera["ProjectionType"].as<std::string>());
-                        float aspectRatio = camera["AspectRatio"].as<float>();
-                        float FOV_or_Zoom = camera["FOV/Zoom"].as<float>();
-                        float near = camera["Near"].as<float>();
-                        float far = camera["Far"].as<float>();
+                    ProjectionType type = ProjectionTypeStringToEnum(camera["ProjectionType"].as<std::string>());
+                    float aspectRatio = camera["AspectRatio"].as<float>();
+                    float FOV_or_Zoom = camera["FOV/Zoom"].as<float>();
+                    float near = camera["Near"].as<float>();
+                    float far = camera["Far"].as<float>();
 
-                        switch (type)
-                        {
-                            case ProjectionType::Orthographic:
-                                cameraComp.Camera.SetOrthographic(aspectRatio, FOV_or_Zoom, near, far);
-                                break;
-                            case ProjectionType::Perspective:
-                                cameraComp.Camera.SetPerspective(aspectRatio, FOV_or_Zoom, near, far);
-                                break;
-                        }
+                    switch (type)
+                    {
+                        case ProjectionType::Orthographic:
+                            cameraComp.Camera.SetOrthographic(aspectRatio, FOV_or_Zoom, near, far);
+                            break;
+                        case ProjectionType::Perspective:
+                            cameraComp.Camera.SetPerspective(aspectRatio, FOV_or_Zoom, near, far);
+                            break;
                     }
+                }
 
-                    if (auto mesh = entity["MeshComponent"]; mesh)
-                    {
-                        auto& meshComp = destScene->m_Registry->emplace<MeshComponent>(deserializedEntity, 0);
-                        meshComp.MeshHandle = mesh["MeshHandle"].as<uint64_t>();
+                if (auto mesh = entity["MeshComponent"]; mesh)
+                {
+                    auto& meshComp = destScene->m_Registry->emplace<MeshComponent>(deserializedEntity, 0);
+                    meshComp.MeshHandle = mesh["MeshHandle"].as<uint64_t>();
 
-                        for (auto entry : mesh["OverridenMaterialTable"]) // TODO: Update with different format
-                        {
-                            auto mat = AssetManager::TryGetOrLoadAsset<Material>(entry["Material"].as<std::string>());
-                            meshComp.OverridenMaterialTable[entry["SubmeshIndex"].as<uint32_t>()] = mat->Handle;
-                        }
-                    }
-                    
-                    if (auto skyLight = entity["SkyLightComponent"]; skyLight)
+                    for (auto entry : mesh["OverridenMaterialTable"]) // TODO: Update with different format
                     {
-                        auto& skyLightComp = destScene->m_Registry->emplace<SkyLightComponent>(deserializedEntity);
-                        skyLightComp.Color = skyLight["Color"].as<glm::vec3>();
-                        skyLightComp.Intensity = skyLight["Intensity"].as<float>();
-                        skyLightComp.EnableSkyMap = skyLight["EnableSkyMap"].as<bool>();
-                        
-                        std::string skymapPath = skyLight["SkyMap"].as<std::string>();
-                        skyLightComp.SkyMap = ((skymapPath != "") ? AssetManager::TryGetOrLoadAsset<Texture2D>(skyLight["SkyMap"].as<std::string>())->Handle : AssetHandle(0));
+                        auto mat = AssetManager::TryGetOrLoadAsset<Material>(entry["Material"].as<std::string>());
+                        meshComp.OverridenMaterialTable[entry["SubmeshIndex"].as<uint32_t>()] = mat->Handle;
                     }
-                    
-                    if (auto light = entity["DirectionalLightComponent"]; light)
-                    {
-                        auto& lightComp = destScene->m_Registry->emplace<DirectionalLightComponent>(deserializedEntity);
-                        lightComp.Color = light["Color"].as<glm::vec3>();
-                        lightComp.Intensity = light["Intensity"].as<float>();
-                        lightComp.LightSize = light["LightSize"].as<float>();
-                    }
+                }
 
-                    if (auto light = entity["PointLightComponent"]; light)
-                    {
-                        auto& lightComp = destScene->m_Registry->emplace<PointLightComponent>(deserializedEntity);
-                        lightComp.Color = light["Color"].as<glm::vec3>();
-                        lightComp.Intensity = light["Intensity"].as<float>();
-                    }
+                if (auto skyLight = entity["SkyLightComponent"]; skyLight)
+                {
+                    auto& skyLightComp = destScene->m_Registry->emplace<SkyLightComponent>(deserializedEntity);
+                    skyLightComp.Color = skyLight["Color"].as<glm::vec3>();
+                    skyLightComp.Intensity = skyLight["Intensity"].as<float>();
+                    skyLightComp.EnableSkyMap = skyLight["EnableSkyMap"].as<bool>();
 
-                    if (auto rigidBody = entity["RigidBodyComponent"]; rigidBody)
-                    {
-                        auto& rbComp = destScene->m_Registry->emplace<RigidBodyComponent>(deserializedEntity);
-                        rbComp.Type = RigidBodyTypeStringToEnum(rigidBody["Type"].as<std::string>());
-                        rbComp.Density = rigidBody["Density"].as<float>();
-                        rbComp.StaticFriction = rigidBody["StaticFriction"].as<float>();
-                        rbComp.DynamicFriction = rigidBody["DynamicFriction"].as<float>();
-                        rbComp.Restitution = rigidBody["Restitution"].as<float>();
-                    }
+                    std::string skymapPath = skyLight["SkyMap"].as<std::string>();
+                    skyLightComp.SkyMap = ((skymapPath != "") ? AssetManager::TryGetOrLoadAsset<Texture2D>(skyLight["SkyMap"].as<std::string>())->Handle : AssetHandle(0));
+                }
 
-                    if (auto boxCollider = entity["BoxColliderComponent"]; boxCollider)
-                    {
-                        auto& bcComp = destScene->m_Registry->emplace<BoxColliderComponent>(deserializedEntity);
-                        bcComp.Size = boxCollider["Size"].as<glm::vec3>();
-                    }
+                if (auto light = entity["DirectionalLightComponent"]; light)
+                {
+                    auto& lightComp = destScene->m_Registry->emplace<DirectionalLightComponent>(deserializedEntity);
+                    lightComp.Color = light["Color"].as<glm::vec3>();
+                    lightComp.Intensity = light["Intensity"].as<float>();
+                    lightComp.LightSize = light["LightSize"].as<float>();
+                }
 
-                    if (auto sphereCollider = entity["SphereColliderComponent"]; sphereCollider)
-                    {
-                        auto& scComp = destScene->m_Registry->emplace<SphereColliderComponent>(deserializedEntity);
-                        scComp.Radius = sphereCollider["Radius"].as<float>();
-                    }
+                if (auto light = entity["PointLightComponent"]; light)
+                {
+                    auto& lightComp = destScene->m_Registry->emplace<PointLightComponent>(deserializedEntity);
+                    lightComp.Color = light["Color"].as<glm::vec3>();
+                    lightComp.Intensity = light["Intensity"].as<float>();
+                }
 
-                    if (auto capsuleCollider = entity["CapsuleColliderComponent"]; capsuleCollider)
-                    {
-                        auto& ccComp = destScene->m_Registry->emplace<CapsuleColliderComponent>(deserializedEntity);
-                        ccComp.Axis = AxisTypeStringToEnum(capsuleCollider["Axis"].as<std::string>());
-                        ccComp.Radius = capsuleCollider["Radius"].as<float>();
-                        ccComp.Height = capsuleCollider["Height"].as<float>();
-                    }
+                if (auto rigidBody = entity["RigidBodyComponent"]; rigidBody)
+                {
+                    auto& rbComp = destScene->m_Registry->emplace<RigidBodyComponent>(deserializedEntity);
+                    rbComp.Type = RigidBodyTypeStringToEnum(rigidBody["Type"].as<std::string>());
+                    rbComp.Density = rigidBody["Density"].as<float>();
+                    rbComp.StaticFriction = rigidBody["StaticFriction"].as<float>();
+                    rbComp.DynamicFriction = rigidBody["DynamicFriction"].as<float>();
+                    rbComp.Restitution = rigidBody["Restitution"].as<float>();
+                }
+
+                if (auto boxCollider = entity["BoxColliderComponent"]; boxCollider)
+                {
+                    auto& bcComp = destScene->m_Registry->emplace<BoxColliderComponent>(deserializedEntity);
+                    bcComp.Size = boxCollider["Size"].as<glm::vec3>();
+                }
+
+                if (auto sphereCollider = entity["SphereColliderComponent"]; sphereCollider)
+                {
+                    auto& scComp = destScene->m_Registry->emplace<SphereColliderComponent>(deserializedEntity);
+                    scComp.Radius = sphereCollider["Radius"].as<float>();
+                }
+
+                if (auto capsuleCollider = entity["CapsuleColliderComponent"]; capsuleCollider)
+                {
+                    auto& ccComp = destScene->m_Registry->emplace<CapsuleColliderComponent>(deserializedEntity);
+                    ccComp.Axis = AxisTypeStringToEnum(capsuleCollider["Axis"].as<std::string>());
+                    ccComp.Radius = capsuleCollider["Radius"].as<float>();
+                    ccComp.Height = capsuleCollider["Height"].as<float>();
                 }
             }
         }
@@ -287,6 +301,23 @@ namespace Flameberry {
             out << YAML::EndMap; // Transform Component
         }
 
+        if (scene->m_Registry->has<RelationshipComponent>(entity))
+        {
+            auto& relation = scene->m_Registry->get<RelationshipComponent>(entity);
+            out << YAML::Key << "RelationshipComponent" << YAML::BeginMap;
+
+            const UUID parentID = relation.Parent != fbentt::null ? scene->m_Registry->get<IDComponent>(relation.Parent).ID : UUID(0);
+            const UUID prevSiblingID = relation.PrevSibling != fbentt::null ? scene->m_Registry->get<IDComponent>(relation.PrevSibling).ID : UUID(0);
+            const UUID nextSiblingID = relation.NextSibling != fbentt::null ? scene->m_Registry->get<IDComponent>(relation.NextSibling).ID : UUID(0);
+            const UUID firstChildID = relation.FirstChild != fbentt::null ? scene->m_Registry->get<IDComponent>(relation.FirstChild).ID : UUID(0);
+
+            out << YAML::Key << "Parent" << YAML::Value << parentID;
+            out << YAML::Key << "PrevSibling" << YAML::Value << prevSiblingID;
+            out << YAML::Key << "NextSibling" << YAML::Value << nextSiblingID;
+            out << YAML::Key << "FirstChild" << YAML::Value << firstChildID;
+            out << YAML::EndMap; // Relationship Component
+        }
+
         if (scene->m_Registry->has<CameraComponent>(entity))
         {
             auto& cameraComp = scene->m_Registry->get<CameraComponent>(entity);
@@ -322,7 +353,7 @@ namespace Flameberry {
 
             meshUUIDs.insert(mesh.MeshHandle);
         }
-        
+
         if (scene->m_Registry->has<SkyLightComponent>(entity))
         {
             auto& skyLight = scene->m_Registry->get<SkyLightComponent>(entity);
@@ -333,7 +364,7 @@ namespace Flameberry {
             out << YAML::Key << "SkyMap" << YAML::Value << (AssetManager::IsAssetHandleValid(skyLight.SkyMap) ? AssetManager::GetAsset<Texture2D>(skyLight.SkyMap)->FilePath : "");
             out << YAML::EndMap; // Sky Light Component
         }
-        
+
         if (scene->m_Registry->has<DirectionalLightComponent>(entity))
         {
             auto& light = scene->m_Registry->get<DirectionalLightComponent>(entity);
